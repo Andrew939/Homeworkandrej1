bd9e974a5bbe2985e760f5e6964535dac31c1a53
Your Name <you@example.com>
2019-08-26 14:54:55 +0300

    hello

---
+++ src/test/java/TestOdinOdin.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+public class TestOdinOdin {
+
+   @Test
+    public void test() {
+       String imjaceloveka = "andrej";
+       int vozrastCeloveka = 29;
+       napecatatjImjaCeloveka("andrej" , 29);
+       napecatatjImjaCeloveka("Boris", 35);
+    }
+
+
+    private void napecatatjImjaCeloveka(String imjaCeloveka, int vozrastCeloveka) {
+        System.out.println("CelovekaZovut" + imjaCeloveka);
+
+        System.out.println("Celoveku" + vozrastCeloveka + "let");
+
+
+    }
+
+
+}
+
+
+
+}
+{
+    public class Andrej{
+    public static void main(String args[]){
+        System.out.println("Hello world!!!");
+    }
+        }
